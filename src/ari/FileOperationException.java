3c7a4cfc6e40a529c3e3ede77cc625c5dd1012a3
Gourav <gppaul998@gmail.com>
2026-09-10 04:18:45 +0600

    Finalize complete project files

--- src/ari/FileOperationException.java
+++ src/ari/FileOperationException.java
@@ -2,7 +2,5 @@ package ari;
 
 public class FileOperationException extends Exception {
     public FileOperationException(String message) { super(message); }
-    public FileOperationException(String operation, String fileName, Throwable cause) {
-        super(String.format("Failed to %s file '%s': %s", operation, fileName, cause.getMessage()), cause);
-    }
+    public FileOperationException(String operation, String fileName, Throwable cause) { super(String.format("Failed to %s file '%s': %s", operation, fileName, cause.getMessage()), cause); }
 }
